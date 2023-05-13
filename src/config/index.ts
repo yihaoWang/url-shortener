bdcaeb65c0ff2435d67e744b0646b8dc9799e1c6
yihaowang <yihao.wang@goanimate.com>
2023-05-13 15:11:15 +0800

    Fix typo

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,10 @@ interface IConfig {
   appDomain: string,
 }
 
-const DEFAULT_ADD_DOMAIN = 'http://localhost:3000';
+const DEFAULT_APP_DOMAIN = 'http://localhost:3000';
 const config: IConfig = {
   instanceId: parseInt(process.env.INSTANCE_ID || '0', 10),
-  appDomain: process.env.INSTANCE_ID || DEFAULT_ADD_DOMAIN,
+  appDomain: process.env.URL_SHORTENER_DOMAIN || DEFAULT_APP_DOMAIN,
 };
 
 export default config;
