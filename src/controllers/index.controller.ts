fd9cb30c99fa0ffaf2d4903d17842872fa067e9c
yihaowang <yihao.wang@goanimate.com>
2023-05-13 09:07:32 +0800

    Add home page

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   rednerHomePage(req: Request, res: Response, next: NextFunction) {
     try {
-      res.json({ message: 'Helo World!' });
+      res.render('index');
     } catch (err) {
       next(err);
     }
